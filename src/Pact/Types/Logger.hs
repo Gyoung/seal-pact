739037558c64d6f0bc13e03f06de1542557b0eef
gyoung <652105072@qq.com>
2018-11-29 15:28:11 +0800

    fix bug

--- src/Pact/Types/Logger.hs
+++ src/Pact/Types/Logger.hs
@@ -119,15 +119,7 @@ runLogIO logger a = runReaderT (logIO a) logger
 #if !defined(ghcjs_HOST_OS)
 _test :: IO ()
 _test = do
-  let config = "Default: {}       \n" <>
-               "Disabled:         \n" <>
-               "  enable: false   \n" <>
-               "Enabled:          \n" <>
-               "  enable: true    \n" <>
-               "IncludeINFO:      \n" <>
-               "  include: [INFO] \n" <>
-               "ExcludeINFO:      \n" <>
-               "  exclude: [INFO]"
+  let config = "Default: {}[INFO]"
       rules = either (error . Y.prettyPrintParseException) id $ Y.decodeEither' config
       loggers = initLoggers putStrLn doLog rules
   forM_ (sort $ HM.keys (logRules rules)) $ \ln -> runLogIO (newLogger loggers ln) _stuff
